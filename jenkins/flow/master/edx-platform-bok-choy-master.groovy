a91dd435bf5dff086bbb23484ce51e579b890cf8
Ben Patterson <bpatterson@edx.org>
2015-10-19 10:06:17 -0400

    Add ENV_VARS.

--- jenkins/flow/master/edx-platform-bok-choy-master.groovy
+++ jenkins/flow/master/edx-platform-bok-choy-master.groovy
@@ -7,7 +7,7 @@ def jenkinsUrl = build.environment.get("JENKINS_URL")
 def jobUrl = jenkinsUrl + build.url
 
 try{
-  def statusJobParams = [
+  def statusJobparams = [
     new StringParameterValue("GITHUB_ORG", "edx"),
     new StringParameterValue("GITHUB_REPO", "edx-platform"),
     new StringParameterValue("GIT_SHA", "${sha1}"),
@@ -21,7 +21,7 @@ try{
   statusJob.scheduleBuild2(
       0,
       new Cause.UpstreamCause(build),
-      new ParametersAction(statusJobParams)
+      new ParametersAction(statusJobparams)
   )
 
   println "Triggered github-build-status"
@@ -29,31 +29,31 @@ try{
   guard{
     parallel(
         {
-          bok_choy_1 = build('edx-platform-test-subset', sha1: sha1, SHARD: "1", TEST_SUITE: "bok-choy", PARENT_BUILD: "master #" + build.number)
+          bok_choy_1 = build('edx-platform-test-subset', sha1: sha1, SHARD: "1", TEST_SUITE: "bok-choy", ENV_VARS: params["ENV_VARS"], PARENT_BUILD: "master #" + build.number)
           toolbox.slurpArtifacts(bok_choy_1)
         },
         {
-          bok_choy_2 = build('edx-platform-test-subset', sha1: sha1, SHARD: "2", TEST_SUITE: "bok-choy", PARENT_BUILD: "master #" + build.number)
+          bok_choy_2 = build('edx-platform-test-subset', sha1: sha1, SHARD: "2", TEST_SUITE: "bok-choy", ENV_VARS: params["ENV_VARS"], PARENT_BUILD: "master #" + build.number)
           toolbox.slurpArtifacts(bok_choy_2)
         },
         {
-          bok_choy_3 = build('edx-platform-test-subset', sha1: sha1, SHARD: "3", TEST_SUITE: "bok-choy", PARENT_BUILD: "master #" + build.number)
+          bok_choy_3 = build('edx-platform-test-subset', sha1: sha1, SHARD: "3", TEST_SUITE: "bok-choy", ENV_VARS: params["ENV_VARS"], PARENT_BUILD: "master #" + build.number)
           toolbox.slurpArtifacts(bok_choy_3)
         },
         {
-          bok_choy_4 = build('edx-platform-test-subset', sha1: sha1, SHARD: "4", TEST_SUITE: "bok-choy", PARENT_BUILD: "master #" + build.number)
+          bok_choy_4 = build('edx-platform-test-subset', sha1: sha1, SHARD: "4", TEST_SUITE: "bok-choy", ENV_VARS: params["ENV_VARS"], PARENT_BUILD: "master #" + build.number)
           toolbox.slurpArtifacts(bok_choy_4)
         },
         {
-          bok_choy_5 = build('edx-platform-test-subset', sha1: sha1, SHARD: "5", TEST_SUITE: "bok-choy", PARENT_BUILD: "master #" + build.number)
+          bok_choy_5 = build('edx-platform-test-subset', sha1: sha1, SHARD: "5", TEST_SUITE: "bok-choy", ENV_VARS: params["ENV_VARS"], PARENT_BUILD: "master #" + build.number)
           toolbox.slurpArtifacts(bok_choy_5)
         },
         {
-          bok_choy_6 = build('edx-platform-test-subset', sha1: sha1, SHARD: "6", TEST_SUITE: "bok-choy", PARENT_BUILD: "master #" + build.number)
+          bok_choy_6 = build('edx-platform-test-subset', sha1: sha1, SHARD: "6", TEST_SUITE: "bok-choy", ENV_VARS: params["ENV_VARS"], PARENT_BUILD: "master #" + build.number)
           toolbox.slurpArtifacts(bok_choy_6)
         },
         {
-          bok_choy_7 = build('edx-platform-test-subset', sha1: sha1, SHARD: "7", TEST_SUITE: "bok-choy", PARENT_BUILD: "master #" + build.number)
+          bok_choy_7 = build('edx-platform-test-subset', sha1: sha1, SHARD: "7", TEST_SUITE: "bok-choy", ENV_VARS: params["ENV_VARS"], PARENT_BUILD: "master #" + build.number)
           toolbox.slurpArtifacts(bok_choy_7)
         },
     )
