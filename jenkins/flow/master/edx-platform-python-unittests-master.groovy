04569c2604c78d3153df3c8de51e58071dabdbcb
Ben Patterson <bpatterson@edx.org>
2016-03-02 08:47:26 -0500

    Shard 3 for lms unit tests.

--- jenkins/flow/master/edx-platform-python-unittests-master.groovy
+++ jenkins/flow/master/edx-platform-python-unittests-master.groovy
@@ -37,6 +37,10 @@ try{
         toolbox.slurpArtifacts(lms_unit_2)
       },
       {
+        lms_unit_3 = build('edx-platform-test-subset', sha1: sha1, SHARD: "3", TEST_SUITE: "lms-unit", PARENT_BUILD: "master #" + build.number)
+        toolbox.slurpArtifacts(lms_unit_3)
+      },
+      {
         cms_unit = build('edx-platform-test-subset', sha1: sha1, SHARD: "1", TEST_SUITE: "cms-unit", PARENT_BUILD: "master #" + build.number)
         toolbox.slurpArtifacts(cms_unit)
       },
@@ -49,6 +53,7 @@ try{
     check_coverage = (
       lms_unit_1.result.toString() == 'SUCCESS' &&
       lms_unit_2.result.toString() == 'SUCCESS' &&
+      lms_unit_3.result.toString() == 'SUCCESS' &&
       cms_unit.result.toString() == 'SUCCESS' &&
       commonlib_unit.result.toString() == 'SUCCESS')
 
@@ -57,7 +62,8 @@ try{
                             UNIT_BUILD_NUM_1: commonlib_unit.number,
                             UNIT_BUILD_NUM_2: lms_unit_1.number,
                             UNIT_BUILD_NUM_3: lms_unit_2.number,
-                            UNIT_BUILD_NUM_4: cms_unit.number,
+                            UNIT_BUILD_NUM_4: lms_unit_3.number,
+                            UNIT_BUILD_NUM_5: cms_unit.number,
                             sha1: sha1,
                             PARENT_BUILD: "master #" + build.number,
                             CI_BRANCH: "master"
