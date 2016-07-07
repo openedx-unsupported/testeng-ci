7d71c753f9cd1862fd4619509958a6e1e7679237
aphanse <aphanse@mit.edu>
2016-07-07 10:47:04 -0400

    For Testing

--- jenkins/flow/pr/edx-platform-bok-choy-pr.groovy
+++ jenkins/flow/pr/edx-platform-bok-choy-pr.groovy
@@ -9,39 +9,39 @@ def repoName = build.environment.get("REPO_NAME") ?: "edx-platform"
 guard{
   parallel(
     {
-      bok_choy_1 = build('edx-platform-test-subset', sha1: sha1, SHARD: "1", TEST_SUITE: "bok-choy", PARENT_BUILD: "PR Build #" + build.number)
+      bok_choy_1 = build(subsetJob + 'TEST', sha1: sha1, SHARD: "1", TEST_SUITE: "bok-choy", PARENT_BUILD: "PR Build #" + build.number)
       toolbox.slurpArtifacts(bok_choy_1)
     },
     {
-      bok_choy_2 = build('edx-platform-test-subset', sha1: sha1, SHARD: "2", TEST_SUITE: "bok-choy", PARENT_BUILD: "PR Build #" + build.number)
+      bok_choy_2 = build(subsetJob + 'TEST', sha1: sha1, SHARD: "2", TEST_SUITE: "bok-choy", PARENT_BUILD: "PR Build #" + build.number)
       toolbox.slurpArtifacts(bok_choy_2)
     },
     {
-      bok_choy_3 = build('edx-platform-test-subset', sha1: sha1, SHARD: "3", TEST_SUITE: "bok-choy", PARENT_BUILD: "PR Build #" + build.number)
+      bok_choy_3 = build(subsetJob + 'TEST', sha1: sha1, SHARD: "3", TEST_SUITE: "bok-choy", PARENT_BUILD: "PR Build #" + build.number)
       toolbox.slurpArtifacts(bok_choy_3)
     },
     {
-      bok_choy_4 = build('edx-platform-test-subset', sha1: sha1, SHARD: "4", TEST_SUITE: "bok-choy", PARENT_BUILD: "PR Build #" + build.number)
+      bok_choy_4 = build(subsetJob + 'TEST', sha1: sha1, SHARD: "4", TEST_SUITE: "bok-choy", PARENT_BUILD: "PR Build #" + build.number)
       toolbox.slurpArtifacts(bok_choy_4)
     },
     {
-      bok_choy_5 = build('edx-platform-test-subset', sha1: sha1, SHARD: "5", TEST_SUITE: "bok-choy", PARENT_BUILD: "PR Build #" + build.number)
+      bok_choy_5 = build(subsetJob + 'TEST', sha1: sha1, SHARD: "5", TEST_SUITE: "bok-choy", PARENT_BUILD: "PR Build #" + build.number)
       toolbox.slurpArtifacts(bok_choy_5)
     },
     {
-      bok_choy_6 = build('edx-platform-test-subset', sha1: sha1, SHARD: "6", TEST_SUITE: "bok-choy", PARENT_BUILD: "PR Build #" + build.number)
+      bok_choy_6 = build(subsetJob + 'TEST', sha1: sha1, SHARD: "6", TEST_SUITE: "bok-choy", PARENT_BUILD: "PR Build #" + build.number)
       toolbox.slurpArtifacts(bok_choy_6)
     },
     {
-      bok_choy_7 = build('edx-platform-test-subset', sha1: sha1, SHARD: "7", TEST_SUITE: "bok-choy", PARENT_BUILD: "PR Build #" + build.number)
+      bok_choy_7 = build(subsetJob + 'TEST', sha1: sha1, SHARD: "7", TEST_SUITE: "bok-choy", PARENT_BUILD: "PR Build #" + build.number)
       toolbox.slurpArtifacts(bok_choy_7)
     },
     {
-      bok_choy_8 = build('edx-platform-test-subset', sha1: sha1, SHARD: "8", TEST_SUITE: "bok-choy", PARENT_BUILD: "PR Build #" + build.number)
+      bok_choy_8 = build(subsetJob + 'TEST', sha1: sha1, SHARD: "8", TEST_SUITE: "bok-choy", PARENT_BUILD: "PR Build #" + build.number)
       toolbox.slurpArtifacts(bok_choy_8)
     },
     {
-      bok_choy_9 = build('edx-platform-test-subset', sha1: sha1, SHARD: "9", TEST_SUITE: "bok-choy", PARENT_BUILD: "PR Build #" + build.number)
+      bok_choy_9 = build(subsetJob + 'TEST', sha1: sha1, SHARD: "9", TEST_SUITE: "bok-choy", PARENT_BUILD: "PR Build #" + build.number)
       toolbox.slurpArtifacts(bok_choy_9)
     },
   )
