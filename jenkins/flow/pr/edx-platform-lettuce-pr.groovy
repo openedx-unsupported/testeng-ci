95e9ee2e7c2c145e5870d460cefe73dff08c4356
aphanse <aphanse@mit.edu>
2016-07-15 11:55:03 -0400

    Update Lettuce Shard Script

--- jenkins/flow/pr/edx-platform-lettuce-pr.groovy
+++ jenkins/flow/pr/edx-platform-lettuce-pr.groovy
@@ -3,20 +3,22 @@ import hudson.model.*
 
 def toolbox = extension."build-flow-toolbox"
 def sha1 = build.environment.get("ghprbActualCommit")
+def subsetJob = build.environment.get("SUBSET_JOB") ?: "edx-platform-test-subset"
+def repoName = build.environment.get("REPO_NAME") ?: "edx-platform"
 
 guard{
   parallel(
     {
-      lettuce_lms = build('edx-platform-test-subset', sha1: sha1, SHARD: "all", TEST_SUITE: "lms-acceptance", PARENT_BUILD: "PR Build #" + build.number)
+      lettuce_lms = build(subsetJob, sha1: sha1, SHARD: "all", TEST_SUITE: "lms-acceptance", PARENT_BUILD: "PR Build #" + build.number)
       toolbox.slurpArtifacts(lettuce_lms)
     },
     {
-      lettuce_cms = build('edx-platform-test-subset', sha1: sha1, SHARD: "all", TEST_SUITE: "cms-acceptance", PARENT_BUILD: "PR Build #" + build.number)
+      lettuce_cms = build(subsetJob, sha1: sha1, SHARD: "all", TEST_SUITE: "cms-acceptance", PARENT_BUILD: "PR Build #" + build.number)
       toolbox.slurpArtifacts(lettuce_cms)
     },
   )
 }rescue{
   FilePath artifactsDir =  new FilePath(build.artifactManager.getArtifactsDir())
-  FilePath copyToDir = new FilePath(build.workspace, "edx-platform")
+  FilePath copyToDir = new FilePath(build.workspace, repoName)
   artifactsDir.copyRecursiveTo(copyToDir)
 }
