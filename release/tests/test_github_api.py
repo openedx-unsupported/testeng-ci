1624c0e9afc9c5233b382f2ae68b4b25847dd236
Brian Beggs <bbeggs@edx.org>
2016-10-14 12:58:39 -0400

    update github-api tests

--- release/tests/test_github_api.py
+++ release/tests/test_github_api.py
@@ -64,7 +64,7 @@ ENDPOINTS = [
         }
     ),
     EndpointInfo(
-        "https://api.github.com/repos/test-org/test-repo/git/refs/test-branch",
+        "https://api.github.com/repos/test-org/test-repo/git/refs/heads/test-branch",
         lambda api: api.delete_branch('test-branch'),
         success=204,
         method=DELETE
