36ed1494e818ea49acd1415c7620facebefe2f2d
Ben Patterson <bpatterson@edx.org>
2016-12-29 11:11:38 -0500

    Use loops in build flow.

--- jenkins/flow/master/edx-platform-bok-choy-master.groovy
+++ jenkins/flow/master/edx-platform-bok-choy-master.groovy
@@ -35,42 +35,13 @@ try{
 } finally{
   guard{
     parallel(
-        {
-          bok_choy_1 = build(subsetJob, sha1: sha1, SHARD: "1", TEST_SUITE: "bok-choy", ENV_VARS: params["ENV_VARS"], PARENT_BUILD: "master #" + build.number, WORKER_LABEL: workerLabel)
-          toolbox.slurpArtifacts(bok_choy_1)
-        },
-        {
-          bok_choy_2 = build(subsetJob, sha1: sha1, SHARD: "2", TEST_SUITE: "bok-choy", ENV_VARS: params["ENV_VARS"], PARENT_BUILD: "master #" + build.number, WORKER_LABEL: workerLabel)
-          toolbox.slurpArtifacts(bok_choy_2)
-        },
-        {
-          bok_choy_3 = build(subsetJob, sha1: sha1, SHARD: "3", TEST_SUITE: "bok-choy", ENV_VARS: params["ENV_VARS"], PARENT_BUILD: "master #" + build.number, WORKER_LABEL: workerLabel)
-          toolbox.slurpArtifacts(bok_choy_3)
-        },
-        {
-          bok_choy_4 = build(subsetJob, sha1: sha1, SHARD: "4", TEST_SUITE: "bok-choy", ENV_VARS: params["ENV_VARS"], PARENT_BUILD: "master #" + build.number, WORKER_LABEL: workerLabel)
-          toolbox.slurpArtifacts(bok_choy_4)
-        },
-        {
-          bok_choy_5 = build(subsetJob, sha1: sha1, SHARD: "5", TEST_SUITE: "bok-choy", ENV_VARS: params["ENV_VARS"], PARENT_BUILD: "master #" + build.number, WORKER_LABEL: workerLabel)
-          toolbox.slurpArtifacts(bok_choy_5)
-        },
-        {
-          bok_choy_6 = build(subsetJob, sha1: sha1, SHARD: "6", TEST_SUITE: "bok-choy", ENV_VARS: params["ENV_VARS"], PARENT_BUILD: "master #" + build.number, WORKER_LABEL: workerLabel)
-          toolbox.slurpArtifacts(bok_choy_6)
-        },
-        {
-          bok_choy_7 = build(subsetJob, sha1: sha1, SHARD: "7", TEST_SUITE: "bok-choy", ENV_VARS: params["ENV_VARS"], PARENT_BUILD: "master #" + build.number, WORKER_LABEL: workerLabel)
-          toolbox.slurpArtifacts(bok_choy_7)
-        },
-        {
-          bok_choy_8 = build(subsetJob, sha1: sha1, SHARD: "8", TEST_SUITE: "bok-choy", ENV_VARS: params["ENV_VARS"], PARENT_BUILD: "master #" + build.number, WORKER_LABEL: workerLabel)
-          toolbox.slurpArtifacts(bok_choy_8)
-        },
-        {
-          bok_choy_9 = build(subsetJob, sha1: sha1, SHARD: "9", TEST_SUITE: "bok-choy", ENV_VARS: params["ENV_VARS"], PARENT_BUILD: "master #" + build.number, WORKER_LABEL: workerLabel)
-          toolbox.slurpArtifacts(bok_choy_9)
-        },
+      (1..9).collect { index ->
+        return {
+          bokchoybuild = build(subsetJob, sha1: sha1, SHARD: index, TEST_SUITE: "bok-choy", ENV_VARS: params["ENV_VARS"], PARENT_BUILD: "master #" + build.number, WORKER_LABEL: workerLabel)
+          toolbox.slurpArtifacts(bokchoybuild)
+
+          }
+        }
     )
   }rescue{
     FilePath artifactsDir =  new FilePath(build.artifactManager.getArtifactsDir())
