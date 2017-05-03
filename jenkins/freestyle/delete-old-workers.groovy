e2c66e1d157a62d365f1a6f0d81d7f7f3f065e81
Ben Patterson <bpatterson@edx.org>
2017-05-03 16:21:46 -0400

    Only delete workers that are not doing anything.

--- jenkins/freestyle/delete-old-workers.groovy
+++ jenkins/freestyle/delete-old-workers.groovy
@@ -7,8 +7,11 @@ jenkins = Hudson.instance
 
 for (slave in jenkins.computers) {
     def computer = slave.name
-    if (slave.name != "master" & slave.isOffline() & slave.offlineCause != null) {
+    // filter for workers that are offline, are not processing anything, and have some description on why
+    // they are offline
+    if (slave.name != "master" & slave.isOffline() & slave.offlineCause != null & slave.countBusy() == 0) {
         offCause = slave.getOfflineCause().toString()
+        // another filter for a specific offline reason.
         if (offCause.contains("Time out for last 5 try")) {
             println("Deleting " + slave.name + " which has the status: " + slave.offlineCause)
             slave.doDoDelete()
