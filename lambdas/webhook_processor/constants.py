800e9ebe7e75ed9034e0363f4d384a410f2fa131
Michael Youngstrom <myoungstrom@edx.org>
2017-05-30 15:49:54 -0400

    Add constants

---
+++ lambdas/webhook_processor/constants.py
@@ -0,0 +1,53 @@
+EDX_PLATFORM_MASTER = [
+    "edx-platform-accessibility-master",
+    "edx-platform-bok-choy-master",
+    "edx-platform-js-master",
+    "edx-platform-lettuce-master",
+    "edx-platform-python-unittests-master",
+    "edx-platform-quality-master",
+]
+
+EDX_PLATFORM_PR = [
+    "edx-platform-accessibility-pr",
+    "edx-platform-bok-choy-pr",
+    "edx-platform-js-pr",
+    "edx-platform-lettuce-pr",
+    "edx-platform-python-unittests-pr",
+    "edx-platform-quality-pr",
+]
+
+EDX_PLATFORM_EUCALYPTUS_MASTER = [
+    "eucalyptus-accessibility-master",
+    "eucalyptus-bok-choy-master",
+    "eucalyptus-js-master",
+    "eucalyptus-lettuce-master",
+    "eucalyptus-python-unittests-master",
+    "eucalyptus-platform-quality-master",
+] 
+
+EDX_PLATFORM_EUCALYPTUS_PR = [
+    "eucalyptus-accessibility-pr",
+    "eucalyptus-bok-choy-pr",
+    "eucalyptus-js-pr",
+    "eucalyptus-lettuce-pr",
+    "eucalyptus-python-unittests-pr",
+    "eucalyptus-platform-quality-pr",
+]
+
+EDX_PLATFORM_FICUS_MASTER = [
+    "ficus-accessibility-master",
+    "ficus-bok-choy-master",
+    "ficus-js-master",
+    "ficus-lettuce-master",
+    "ficus-python-unittests-master",
+    "ficus-platform-quality-master",
+]
+
+EDX_PLATFORM_FICUS_PR = [
+    "ficus-accessibility-pr",
+    "ficus-bok-choy-pr",
+    "ficus-js-pr",
+    "ficus-lettuce-pr",
+    "ficus-python-unittests-pr",
+    "ficus-platform-quality-pr",
+]
