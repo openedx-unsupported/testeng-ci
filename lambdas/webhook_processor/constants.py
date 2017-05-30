1d1cc88e83da84b97bba6fcf0c54849fc33a3644
Michael Youngstrom <myoungstrom@edx.org>
2017-05-30 17:25:52 -0400

    add tests for platform private and e2e

--- lambdas/webhook_processor/constants.py
+++ lambdas/webhook_processor/constants.py
@@ -7,6 +7,7 @@ EDX_PLATFORM_BASE_TESTS = [
     "-quality-",
 ]
 
+# edx-platform tests
 EDX_PLATFORM_MASTER = ["edx-platform" + base + "master" for base in EDX_PLATFORM_BASE_TESTS]
 EDX_PLATFORM_PR = ["edx-platform" + base + "pr" for base in EDX_PLATFORM_BASE_TESTS]
 
@@ -15,3 +16,13 @@ EDX_PLATFORM_EUCALYPTUS_PR = ["eucalyptus" + base + "pr" for base in EDX_PLATFOR
 
 EDX_PLATFORM_FICUS_MASTER = ["ficus" + base + "master" for base in EDX_PLATFORM_BASE_TESTS]
 EDX_PLATFORM_FICUS_PR = ["ficus" + base + "pr" for base in EDX_PLATFORM_BASE_TESTS]
+
+# edx-platform-private tests
+EDX_PLATFORM_PRIVATE_MASTER = ["edx-platform" + base + "master_private" for base in EDX_PLATFORM_BASE_TESTS]
+EDX_PLATFORM_PRIVATE_PR = ["edx-platform" + base + "pr_private" for base in EDX_PLATFORM_BASE_TESTS]
+
+# edx-e2e-tests
+EDX_E2E_PR = [
+    "microsites-staging-tests-pr",
+    "edx-e2e-tests-pr"
+]
