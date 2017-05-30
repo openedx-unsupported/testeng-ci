8547e5188867a66c79b2517add7b87a31f7b155d
Michael Youngstrom <myoungstrom@edx.org>
2017-05-30 16:01:53 -0400

    clean up constants.py

--- lambdas/webhook_processor/constants.py
+++ lambdas/webhook_processor/constants.py
@@ -1,53 +1,17 @@
-EDX_PLATFORM_MASTER = [
-    "edx-platform-accessibility-master",
-    "edx-platform-bok-choy-master",
-    "edx-platform-js-master",
-    "edx-platform-lettuce-master",
-    "edx-platform-python-unittests-master",
-    "edx-platform-quality-master",
+EDX_PLATFORM_BASE_TESTS = [
+    "-accessibility-",
+    "-bok-choy-",
+    "-js-",
+    "-lettuce-",
+    "-python-unittests-",
+    "-quality-",
 ]
 
-EDX_PLATFORM_PR = [
-    "edx-platform-accessibility-pr",
-    "edx-platform-bok-choy-pr",
-    "edx-platform-js-pr",
-    "edx-platform-lettuce-pr",
-    "edx-platform-python-unittests-pr",
-    "edx-platform-quality-pr",
-]
+EDX_PLATFORM_MASTER = ["edx-platform" + base + "master" for base in EDX_PLATFORM_BASE_TESTS]
+EDX_PLATFORM_PR = ["edx-platform" + base + "pr" for base in EDX_PLATFORM_BASE_TESTS]
 
-EDX_PLATFORM_EUCALYPTUS_MASTER = [
-    "eucalyptus-accessibility-master",
-    "eucalyptus-bok-choy-master",
-    "eucalyptus-js-master",
-    "eucalyptus-lettuce-master",
-    "eucalyptus-python-unittests-master",
-    "eucalyptus-platform-quality-master",
-] 
+EDX_PLATFORM_EUCALYPTUS_MASTER = ["eucalyptus" + base + "master" for base in EDX_PLATFORM_BASE_TESTS]
+EDX_PLATFORM_EUCALYPTUS_PR = ["eucalyptus" + base + "pr" for base in EDX_PLATFORM_BASE_TESTS]
 
-EDX_PLATFORM_EUCALYPTUS_PR = [
-    "eucalyptus-accessibility-pr",
-    "eucalyptus-bok-choy-pr",
-    "eucalyptus-js-pr",
-    "eucalyptus-lettuce-pr",
-    "eucalyptus-python-unittests-pr",
-    "eucalyptus-platform-quality-pr",
-]
-
-EDX_PLATFORM_FICUS_MASTER = [
-    "ficus-accessibility-master",
-    "ficus-bok-choy-master",
-    "ficus-js-master",
-    "ficus-lettuce-master",
-    "ficus-python-unittests-master",
-    "ficus-platform-quality-master",
-]
-
-EDX_PLATFORM_FICUS_PR = [
-    "ficus-accessibility-pr",
-    "ficus-bok-choy-pr",
-    "ficus-js-pr",
-    "ficus-lettuce-pr",
-    "ficus-python-unittests-pr",
-    "ficus-platform-quality-pr",
-]
+EDX_PLATFORM_FICUS_MASTER = ["ficus" + base + "master" for base in EDX_PLATFORM_BASE_TESTS]
+EDX_PLATFORM_FICUS_PR = ["ficus" + base + "pr" for base in EDX_PLATFORM_BASE_TESTS]
