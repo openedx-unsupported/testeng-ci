7b25c0549b0a7676986c3e1d640765fa5d527910
Michael Youngstrom <myoungstrom@edx.org>
2017-05-31 12:42:59 -0400

    Start to add jenkins retry logic

--- lambdas/webhook_processor/webhook_processor.py
+++ lambdas/webhook_processor/webhook_processor.py
@@ -3,6 +3,8 @@ import json
 import logging
 import os
 import urllib
+import re
+from six.moves.urllib.parse import urlparse
 from constants import (
     EDX_PLATFORM_MASTER,
     EDX_PLATFORM_PR,
@@ -44,6 +46,20 @@ def _get_target_urls():
         raise StandardError('Environment variable TARGET_URLS was not set')
 
     return url.split(',')
+
+
+def _get_num_retries():
+    """
+    Get the number of retries desired. If Jenkins does not trigger
+    all of the expected jobs, the webhook will be resent up to
+    this number of times.
+
+    Return a integer
+    """
+    num_retries = os.environ.get('NUM_RETRIES')
+    if not isinstance(num_retries, int):
+        raise StandardError('Environment variable NUM_RETRIES was not set to a valid integer')
+    return num_retries
 
 
 def _verify_data(data_string):
@@ -139,6 +155,158 @@ def _process_results_for_failures(results):
     return results_count
 
 
+def _parse_hook_for_testing_info(data_string):
+    """
+    Parse the webhook to find the commit sha,
+    as well as the arguments needed to find the
+    jobs_list.
+    """
+    repository = 'edx-e2e-tests'
+    target = 'master'
+    event_type = 'pull_request'
+    sha = "bd6106be30c9f72fb0353028cbd189c476a7c7c6"
+    is_merge = False
+    # data_object = json.loads(data_string)
+    # is_merge = False
+
+    # event_type = headers.get('X-GitHub-Event')
+    # if event_type == 'pull_request':
+    #     repository = data_object['pull_request']['head']['repo']['name']
+    #     target = data_object['pull_request']['head']['ref']
+    #     if data_object['action'] == 'opened':
+    #         sha = data_object['pull_request']['head']['sha']
+    #     elif data_object['action'] == 'closed':
+    #         sha = data_object['merge_commit_sha']
+    #         if sha != 'null':
+    #             is_merge = True
+    #             return sha, []
+
+    return sha, _get_jobs_list(repository, target, event_type, is_merge)
+
+
+def _get_jobs_list(repository, target, event_type, is_merge):
+    """
+    Find the list of jobs that the webhook should kick
+    off on Jenkins.
+    """
+    if repository == 'edx-platform':
+        if event_type == 'pull_request':
+            if target == 'master':
+                if is_merge:
+                    return EDX_PLATFORM_MASTER
+                else:
+                    return EDX_PLATFORM_PR
+            if target == 'eucalyptus':
+                if is_merge:
+                    return EDX_PLATFORM_EUCALYPTUS_MASTER
+                else:
+                    return EDX_PLATFORM_EUCALTYPUS_PR
+            if target == 'ficus':
+                if is_merge:
+                    return EDX_PLATFORM_FICUS_MASTER
+                else:
+                    return EDX_PLATFORM_FICUS_PR
+    elif repository == 'edx-platform-private':
+        if event_type == 'pull_request':
+            if is_merge:
+                return EDX_PLATFORM_PRIVATE_MASTER
+            else:
+                return EDX_PLATFORM_PRIVATE_PR
+    elif repository == 'edx-e2e-tests':
+        if event_type == 'pull_request':
+            if not is_merge:
+                return EDX_E2E_PR
+    return []
+
+
+def _all_tests_triggered(jobs_list, sha):
+    """
+    Check to see if the sha has triggered each
+    job in the jobs_list. Looks at both the queue
+    as well as currently running builds
+    """
+    queued_or_running = _get_queued_builds() + _get_running_builds()
+    return _builds_contain_tests(queued_or_running, sha, jobs_list)
+
+
+def _builds_contain_tests(builds, sha, jobs_list):
+    """
+    From the list of all running/queued builds, check
+    to see if the webhook's sha has kicked off every
+    job in the jobs_list
+    """
+    if jobs_list:
+        for build in builds:
+            if build['job_name'] in jobs_list and build['sha'] == sha:
+                index = jobs_list.index(build['job_name'])
+                del jobs_list[index]
+                if len(jobs_list) == 0:
+                    return True
+        return False
+    else:
+        return True
+
+
+def _parse_executables_for_builds(executable, build_status):
+    """
+    Parse executable to find the sha and job name of
+    queued/running builds.
+    """
+    builds = []
+    for action in executable['actions']:
+        if 'parameters' in action:
+            for param in action['parameters']:
+                if param['name'] == 'sha1' or param['name'] == 'ghprbActualCommit':
+                    sha = param['value']
+                    if build_status == 'queued':
+                        job_name = executable['task']['name']
+                    elif build_status == 'running':
+                        url = executable['url']
+                        m = re.search(r'/job/([^/]+)/.*', urlparse(url).path)
+                        job_name = m.group(1)
+                    builds.append({
+                        'job_name': job_name,
+                        'sha': sha
+                    })
+    return builds
+
+
+def _get_queued_builds():
+    """
+    Find all builds currently in the queue
+    """
+    builds = []
+    build_status = 'queued'
+
+    # Use Jenkins REST API to get info on the queue
+    url = '%s/queue/api/json?depth=0' % (BUILD_JENKINS_URL)
+    response = get(url, auth=(JENKINS_USERNAME, BUILD_JENKINS_API_TOKEN)).json()
+
+    # Find all builds in the queue and add them to a list
+    [builds.extend(_parse_executables_for_builds(executable, build_status)) for executable in response['items']]
+    return builds
+
+
+def _get_running_builds():
+    """
+    Find all builds that are currently running
+    """
+    builds = []
+    build_status = 'running'
+
+    # Use Jenkins API to get info on all workers
+    url = '%s/computer/api/json?depth=2' % (BUILD_JENKINS_URL)
+    response = get(url, auth=(JENKINS_USERNAME, BUILD_JENKINS_API_TOKEN)).json()
+
+    # Find all builds being executed and add them to a list
+    for worker in response['computer']:
+        for executor in worker['executors'] + worker['oneOffExecutors']:
+            executable = executor['currentExecutable']
+            if executable:
+                builds.extend(_parse_executables_for_builds(executable, build_status))
+    return builds
+
+
 def lambda_handler(event, _context):
 
     urls = _get_target_urls()
@@ -180,8 +348,26 @@ def lambda_handler(event, _context):
         # Save up the results for later processing rather than letting
         # the errors get raised.
         # That way we can process all records and urls.
+        num_retries = _get_num_retries()
         for url in urls:
-            results.append(_send_message(url, payload, headers))
+            # Check if url is a Jenkins instance
+            if urlparse(url).path contains 'ghprbhook':
+                for attempt in range(0, num_retries):
+                    # send message and check if jobs are triggered
+                    result = _send_message(url, payload, headers)
+                    sha, jobs_list = _parse_hook_for_testing_info(data_string)
+                    if _all_tests_triggered(jobs_list, sha):
+                        logger.info("All Jenkins jobs have been triggered for sha: '{}'".format(sha))
+                        results.append(result)
+                    else:
+                        logger.info("The following Jenkins jobs were not triggered: '{}'".format(jobs_list))
+                        if attempt == num_retries - 1:
+                            # additional action for failures here
+                            results.append(result)
+                        else:
+                            logger.info("Resending webhook...")
+            else:
+                results.append(_send_message(url, payload, headers))
 
     results_count = _process_results_for_failures(results)
 
