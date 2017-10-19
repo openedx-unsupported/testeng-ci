ff00387e589163fff4af7cf5d7c89ce87a1442a0
Jesse Zoldak <zoldak@edx.org>
2017-10-19 09:25:50 -0400

    Action parameters placement in the dict is not deterministic in Jenkins 2.0

--- jenkins/build.py
+++ jenkins/build.py
@@ -19,21 +19,24 @@ class Build(dict):
 
         author = None
         pr_id = None
-        actions = build.get('actions')
-        if actions:
-            action_parameters = actions[0].get('parameters')
-            if action_parameters:
-                for p in action_parameters:
-                    if p.get('name') == u'ghprbActualCommitAuthorEmail':
-                        author = p.get('value')
-                    if p.get('name') == u'ghprbPullId':
-                        pr_id = p.get('value')
-            else:
-                logger.debug(
-                    "Couldn't find build parameters for build #{}".format(
-                        build.get('number')
+        actions = build.get('actions', [])
+        ghprb_class_name = u'org.jenkinsci.plugins.ghprb.GhprbParametersAction'
+
+        for action in actions:
+            if action.get('_class') == ghprb_class_name:
+                action_parameters = action.get('parameters')
+                if action_parameters:
+                    for p in action_parameters:
+                        if p.get('name') == u'ghprbActualCommitAuthorEmail':
+                            author = p.get('value')
+                        if p.get('name') == u'ghprbPullId':
+                            pr_id = p.get('value')
+                else:
+                    logger.debug(
+                        "Couldn't find build parameters for build #{}".format(
+                            build.get('number')
+                        )
                     )
-                )
 
         self.author = author
         self.pr_id = pr_id
