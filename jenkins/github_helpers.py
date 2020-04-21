2bb3cca44bf39f3c7d0fe4679126bd61c4e599de
jinder1s <msingh@edx.org>
2020-04-21 12:05:14 -0400

    Adding print statement

--- jenkins/github_helpers.py
+++ jenkins/github_helpers.py
@@ -62,6 +62,7 @@ def connect_to_repo(github_instance, repo_name):
     Get the repository object of the desired repo.
     """
     repos_list = github_instance.get_user().get_repos()
+    print(repos_list)
     repository = None
     for repo in repos_list:
         if repo.name == repo_name:
