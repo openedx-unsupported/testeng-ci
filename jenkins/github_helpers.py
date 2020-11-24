b71db6afe1119ba08ebda7f499f02960e6f4a256
Tim McCormack <tmccormack@edx.org>
2020-11-24 00:56:28 +0000

    Simplify and fix regex in `is_cleanup_pr`
    This partially addresses a hardcoded branch name that now has no outside
    usages of the variable.
    
    - Need fullmatch instead of just match
    - Don't return match object; that's too much information for what the
      callers actually want
    - Inline the branch name rather than fixing the lack of re.escape
    
    I'm not entirely sure of the intent here. It looks like this does some
    kind of special-casing for the script run by cleanupPythonCode in
    jenkins-job-dsl, which uses this base branch name.
    
    There are two usages:
    
    - I can remove the call in `close_existing_pull_requests`, since the
      intent there seems to be to avoid closing existing cleanup PRs. This
      makes sense, since there can be multiple cleanup PRs with different
      parameters. (I think I would have preferred to hash the arguments and
      use that for finding duplicate/replacement PRs, but whatever.) However,
      the cleanup job already uses --no-delete-old-pull-requests so this guard
      can be removed.
    - The other one seems to exempt cleanup PRs from failure if tagging fails.
      This also kind of makes sense, since cleanup jobs are generally driven
      by humans rather than automated on a schedule; the human will still see
      the job even if tagging fails. This is a very fragile mechanism, though,
      so I'm leaving a comment for a better approach in the future.

--- jenkins/github_helpers.py
+++ jenkins/github_helpers.py
@@ -9,8 +9,6 @@ import re
 from git import Git, Repo
 from github import Github, GithubObject, InputGitAuthor, InputGitTreeElement
 
-CODE_CLEANUP_BRANCH_NAME = "cleanup-python-code"
-
 logging.basicConfig()
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
@@ -146,7 +144,7 @@ class GitHubHelper:  # pylint: disable=missing-class-docstring
         return branch_object
 
     def is_cleanup_pr(self, branch_name):
-        return re.match("(jenkins/{})-[a-zA-Z0-9]*".format(CODE_CLEANUP_BRANCH_NAME), branch_name)
+        return re.fullmatch("jenkins/cleanup-python-code-[a-zA-Z0-9]*", branch_name) is not None
 
     def close_existing_pull_requests(self, repository, user_login, user_name):
         """
@@ -159,8 +157,6 @@ class GitHubHelper:  # pylint: disable=missing-class-docstring
             user = pr.user
             if user.login == user_login and user.name == user_name:
                 branch_name = pr.head.ref
-                if self.is_cleanup_pr(branch_name):
-                    continue
                 logger.info("Deleting PR: #{}".format(pr.number))
                 pr.create_issue_comment("Closing obsolete PR.")
                 pr.edit(state="closed")
@@ -206,6 +202,8 @@ class GitHubHelper:  # pylint: disable=missing-class-docstring
         """
         Assert if the reviewers we requested were tagged on the PR for review
         """
+        # TODO: Remove is_cleanup_pr in favor of a new CLI option on
+        # PullRequestCreator that makes tagging failures log-only
         if self.is_cleanup_pr(pull_request.head.ref):
             return
         tagged_for_review = pull_request.get_review_requests()
