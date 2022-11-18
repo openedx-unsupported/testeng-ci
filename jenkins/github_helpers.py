802e67f64d490fada16e323c3760a14711d35aeb
Awais Qureshi <awais.qureshi@arbisoft.com>
2022-11-18 18:26:44 +0500

    build: Accessing private repo diff changes.

--- jenkins/github_helpers.py
+++ jenkins/github_helpers.py
@@ -220,8 +220,8 @@ class GitHubHelper:  # pylint: disable=missing-class-docstring
             ) from e
 
         # it's a discovery work that's why only enabled for repo-health-data.
-        # if pull_request.title == 'Python Requirements Update' and repository.name in public_repos_list:
-        self.verify_upgrade_packages(pull_request)
+        if pull_request.title == 'Python Requirements Update' and repository.name in public_repos_list:
+            self.verify_upgrade_packages(pull_request)
 
         return pull_request
 
